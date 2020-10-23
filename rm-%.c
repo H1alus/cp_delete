0ac88a4069b3438177bc44387e89622c2126b4d9
Vittorio Folino <48564957+H1alus@users.noreply.github.com>
2020-10-23 23:48:39 +0200

    Update rm-%.c

--- rm-%.c
+++ rm-%.c
@@ -2,18 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main (int argc, char *argv[]) {
-        char value[2];
-        char c;
-        for (int i = 0; i < strlen(argv[1]); i++) {
-            if (argv[1][i] == '%') {
-                value[0] = argv[1][i + 1];
-                value[1] = argv[1][i + 2];
-                long int int_v = strtol(value, NULL, 16);
-                printf("%c", int_v);
-                i = i + 2;
-            } else {
-                printf("%c", argv[1][i]);
-            }
+int main (int argc, char *argv[]) {  
+    char value[2];
+    char c;
+    for (int i = 0; i < strlen(argv[1]); i++) {
+        if (argv[1][i] == '%') {
+            value[0] = argv[1][i + 1];
+            value[1] = argv[1][i + 2];
+            long int int_v = strtol(value, NULL, 16);
+            printf("%c", int_v);
+            i = i + 2;
+        } else {
+            printf("%c", argv[1][i]);
+        }
     }
 }
