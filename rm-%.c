9c05d413a9ddf6b1919a90cb830f8b3b118b7153
Vittorio Folino <48564957+H1alus@users.noreply.github.com>
2020-10-23 23:46:23 +0200

    Update rm-%.c

--- rm-%.c
+++ rm-%.c
@@ -2,31 +2,18 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define NAME_LENGTH 10000
-
 int main (int argc, char *argv[]) {
-    char *name = (char *) malloc(NAME_LENGTH);
-#ifdef ARG
-    strcpy(name, argv[1]);
-#endif
-#ifndef ARG
-    fgets(name, NAME_LENGTH, stdin);
-#endif
-    char value[3];
-    int int_v;
-
-    for (int i = 0; i < strlen(name); i++) {
-        if (name[i] == '%') {
-            value[0] = name[i + 1];
-            value[1] = name[i + 2];
-            value[2] = '\0';
-            int_v = (int) strtol(value, NULL, 16);
-            printf("%c", int_v);
-            i = i + 2;
-        }
-        else if(name[i] != '%')  {
-            printf("%c", name[i]);
-        }
+        char value[2];
+        char c;
+        for (int i = 0; i < strlen(argv[1]); i++) {
+            if (argv[1][i] == '%') {
+                value[0] = argv[1][i + 1];
+                value[1] = argv[1][i + 2];
+                long int int_v = strtol(value, NULL, 16);
+                printf("%c", int_v);
+                i = i + 2;
+            } else {
+                printf("%c", argv[1][i]);
+            }
     }
-    free(name);
 }
